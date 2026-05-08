eddd7a5ec6c3f10fb4e3582606e173bd2af4463d
Pedro Riera <160364938+Peterz15@users.noreply.github.com>
2026-05-08 17:08:29 -0400

    Sincronización de Semáforos
    Sincronización de intersección: Flujo vehicular y peatonal seguro

--- Semaforo_Arduino_IUJO.ino
+++ Semaforo_Arduino_IUJO.ino
@@ -13,34 +13,64 @@ const int y2 = 4;
 const int r2 = 3;
 const int b2 = 2;
 
+//Semaforo 1
+const int gLed = 13;
+const int yLed = 12;
+const int rLed = 11;
+const int bLed = 10;
+
+const int g1 = 9;
+const int y1 = 8;
+const int r1 = 7;
+const int b1 = 6;
+
+const int g2 = 5;
+const int y2 = 4;
+const int r2 = 3;
+const int b2 = 2;
+
 void setup() {
-  pinMode(rLed, OUTPUT);
-  pinMode(yLed, OUTPUT);
-  pinMode(gLed, OUTPUT);
-  pinMode(bLed, OUTPUT);
-  
-  digitalWrite(rLed, LOW);
-  digitalWrite(yLed, LOW);
-  digitalWrite(gLed, LOW);
-  digitalWrite(bLed, LOW);
+  for (int i = 2; i <= 13; i++) {
+    pinMode(i, OUTPUT);
+    digitalWrite(i, LOW); 
+  }
 }
 
 void loop() {
-  // Secuencia Vehicular: Verde -> Amarillo -> Rojo
-  digitalWrite(gLed, HIGH);
-  digitalWrite(rLed, LOW);
-  delay(5000); 
-
-  digitalWrite(gLed, LOW);
-  digitalWrite(yLed, HIGH);
-  delay(2000);
-  digitalWrite(yLed, LOW);
-
-  digitalWrite(rLed, HIGH);
+// Secuencia Vehicular
+  digitalWrite(gLed, HIGH); 
+  digitalWrite(r1, HIGH); digitalWrite(r2, HIGH); 
+  digitalWrite(bLed, LOW);
+  digitalWrite(b1, HIGH);  digitalWrite(b2, HIGH); 
   delay(5000);
   
-  digitalWrite(rLed, HIGH);
+  digitalWrite(gLed, LOW);
+  digitalWrite(yLed, HIGH); 
+  delay(2000);
+  digitalWrite(yLed, LOW);
+  digitalWrite(rLed, HIGH); 
+
+  digitalWrite(r1, LOW); 
+  digitalWrite(g1, HIGH);
+  digitalWrite(b1, LOW);   
   digitalWrite(bLed, HIGH); 
   delay(5000);
-  digitalWrite(bLed, LOW);
+
+  digitalWrite(g1, LOW);
+  digitalWrite(y1, HIGH); 
+  delay(2000);
+  digitalWrite(y1, LOW);
+  digitalWrite(r1, HIGH);
+
+  digitalWrite(r2, LOW);
+  digitalWrite(g2, HIGH);
+  digitalWrite(b2, LOW);    
+  digitalWrite(b1, HIGH);   
+  delay(5000);
+
+  digitalWrite(g2, LOW);
+  digitalWrite(y2, HIGH);
+  delay(2000);
+  digitalWrite(y2, LOW);
+  digitalWrite(r2, HIGH); 
 }
