0fe031d75e4204fd610b8d060a6bf056c554446a
Pedro Riera <160364938+Peterz15@users.noreply.github.com>
2026-05-08 17:05:03 -0400

    Semáforos 2 y 3
    Agregadas variables y configuración de pines para Semáforos 2 y 3

--- Semaforo_Arduino_IUJO.ino
+++ Semaforo_Arduino_IUJO.ino
@@ -3,6 +3,16 @@ const int yLed = 12;
 const int rLed = 11;
 const int bLed = 10;
 
+const int g1 = 9;
+const int y1 = 8;
+const int r1 = 7;
+const int b1 = 6;
+
+const int g2 = 5;
+const int y2 = 4;
+const int r2 = 3;
+const int b2 = 2;
+
 void setup() {
   pinMode(rLed, OUTPUT);
   pinMode(yLed, OUTPUT);
